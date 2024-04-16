8b3808d57086aadc9a2ce74e6e3e9fe9e5ef51f3
tdddt <doto1116@ewhain.net>
2024-04-16 11:43:23 +0900

    2024/04/16

--- BOJ14225.py
+++ BOJ14225.py
@@ -3,28 +3,34 @@
 # 수열 S가 주어졌을 때,
 # 수열 S의 부분 수열의 합으로 나올 수 "없는" 가장 작은 자연수를 구하는 프로그램
 
+import heapq
+
 N = int(input())
 S = list(map(int,input().split()))
 canSum = []
-
-# for 문 돌면서 못 만들면 출력하기
-# 1. 수열에 있는 경우 2. 수3열의 합으로 만들 수 있는 경우
 
 # 가능한 수열의 합을 조합으로 미리 계산하기
 from itertools import combinations
 for i in range(1,N+1):
     temp = list((combinations(S,i)))
-    for j in range(len(temp)):
-        canSum.append(sum(temp[j]))
+    for j in temp:
+        heapq.heappush(canSum,sum(j))
 
-# canSum.sort()
+ans = canSum[-1]+1
+# canSum = list(set(canSum)) # 중복제거
 
-i = 1
-while(True):
-    if i in canSum:
+i=1
+last = 0 # 중복 판단
+while(len(canSum)>0):
+    temp = heapq.heappop(canSum)
+    if(last==temp):
+        continue
+    if i == temp:
         i+=1
+        last = temp
+        continue
     else:
-        print(i)
+        ans=i
         break
-
     
+print(ans)
